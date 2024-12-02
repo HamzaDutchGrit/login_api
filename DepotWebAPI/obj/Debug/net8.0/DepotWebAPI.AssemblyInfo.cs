4b960678877f35a70e43693ed96eb09ced4117b8
Hamzq <hamza@dutchgrit.nl>
2024-12-02 11:41:09 +0100

    .

--- DepotWebAPI/obj/Debug/net8.0/DepotWebAPI.AssemblyInfo.cs
+++ DepotWebAPI/obj/Debug/net8.0/DepotWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DepotWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d3d530d3322907600e08597a4af116966eefbdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DepotWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DepotWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
